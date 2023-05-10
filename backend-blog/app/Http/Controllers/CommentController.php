8c5c538a12c9f8525df9fe0af955a960cb00771e
Angel Montes <angelmontes963@gmail.com>
2023-05-10 02:44:34 -0600

    Modificación  Se crea CRUD comment

--- backend-blog/app/Http/Controllers/CommentController.php
+++ backend-blog/app/Http/Controllers/CommentController.php
@@ -2,63 +2,100 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\Comment\IndexRequest;
+use App\Http\Requests\Comment\StoreRequest;
+use App\Http\Requests\Comment\ShowRequest;
+use App\Http\Requests\Comment\UpdateRequest;
+use App\Http\Requests\Comment\DeleteRequest;
+
+use App\Models\Comment;
+
 
 class CommentController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Create a new controller instance.
      */
-    public function index()
+    public function __construct()
     {
-        //
+        $this->middleware('auth:sanctum');
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Display a listing of the resource.
      */
-    public function create()
+    public function index(IndexRequest $request)
     {
-        //
+        
+        $comments = Comment::paginate();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $comments
+        ], 201);
+
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreRequest $request)
     {
-        //
+        $comment = new Comment();
+        $comment->id_user = $request->user()->id;
+        $comment->id_publication = $request->id_publication;
+        $comment->content = $request->content;
+        $comment->save();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $comment
+        ], 201);
+
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(ShowRequest $request)
     {
-        //
-    }
+        $comment = Comment::find($request->id);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
+        return response()->json([
+            'status' => 'success',
+            'data' => $comment
+        ], 201);
+
+
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateRequest $request)
     {
-        //
+        $comment = Comment::find($request->id);
+        $comment->content = $request->content;
+        $comment->save();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $comment
+        ], 201);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(DeleteRequest $request)
     {
-        //
+        $comment = Comment::find($request->id);
+        $comment->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $comment
+        ], 201);
     }
+    
 }
