87321d95434e6c887c47d8ae5add5745691b8d7a
Angel Montes <angelmontes963@gmail.com>
2023-05-10 02:16:44 -0600

    Creación  Se crea request store publication

---
+++ backend-blog/app/Http/Requests/Publication/StoreRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\Publication;
+
+use App\Http\Requests\BaseRequest;
+
+class StoreRequest extends BaseRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255|unique:publications',
+            'content' => 'required|string',
+        ];
+    }
+}
