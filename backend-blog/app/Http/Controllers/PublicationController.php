8e3c4ea920a478debb8f5c2ab574d7a9fa47ae53
Angel Montes <angelmontes963@gmail.com>
2023-05-10 02:15:53 -0600

    Modificación  Se crea store  Se crea show  Se crea update  Se crea destroy

--- backend-blog/app/Http/Controllers/PublicationController.php
+++ backend-blog/app/Http/Controllers/PublicationController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Publication\IndexRequest;
+use App\Http\Requests\Publication\StoreRequest;
+use App\Http\Requests\Publication\ShowRequest;
+use App\Http\Requests\Publication\UpdateRequest;
+use App\Http\Requests\Publication\DeleteRequest;
+
 use App\Http\Resources\PublicationResource;
 use Illuminate\Http\Request;
 use App\Models\Publication;
@@ -32,32 +37,63 @@ class PublicationController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreRequest $request)
     {
-        //
+        $publication = new Publication;
+        $publication->title = $request->title;
+        $publication->content = $request->content;
+        $publication->save();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => new PublicationResource($publication)
+        ], 201);
+        
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(ShowRequest $request)
     {
-        //
+        $publication = Publication::findOrFail($request->id);
+
+        return response()->json([
+            'status' => 'success',
+            'data' => new PublicationResource($publication)
+        ], 200);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateRequest $request)
     {
-        //
+        $publication = Publication::findOrFail($request->id);
+
+        $publication->title = $request->title;
+        $publication->content = $request->content;
+        $publication->save();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => new PublicationResource($publication)
+        ], 200);
+        
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(DeleteRequest $request)
     {
-        //
+        $publication = Publication::findOrFail($request->id);
+        $publication->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => new PublicationResource($publication)
+        ], 200);
+
     }
 }
