01c60e406b6ca193aa6836433fcaf1085c91cb4b
Angel Montes <angelmontes963@gmail.com>
2023-05-10 02:43:53 -0600

    Modificación  Se agregan parametros a request show comment

--- backend-blog/app/Http/Requests/Comment/ShowRequest.php
+++ backend-blog/app/Http/Requests/Comment/ShowRequest.php
@@ -11,7 +11,7 @@ class ShowRequest extends BaseRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,7 @@ class ShowRequest extends BaseRequest
     public function rules(): array
     {
         return [
-            //
+            'id' => 'required|exists:comments,id'
         ];
     }
 }
