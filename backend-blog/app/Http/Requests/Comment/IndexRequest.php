0b72baff775ccd35f70139a639e91fabf4d4e00a
Angel Montes <angelmontes963@gmail.com>
2023-05-10 01:00:18 -0600

    Modificación  Se extendiende a clase BaseRequest

--- backend-blog/app/Http/Requests/Comment/IndexRequest.php
+++ backend-blog/app/Http/Requests/Comment/IndexRequest.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Requests\Comment;
 
-use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\BaseRequest;
 
-class IndexRequest extends FormRequest
+class IndexRequest extends BaseRequest
 {
     /**
      * Determine if the user is authorized to make this request.
